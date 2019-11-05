0af84f34dd65ecb519020be88be785f08b16a4bf
Lily <t101570302@gmail.com>
2019-11-05 11:00:40 +0800

    Changed the Template of the site.

--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,17 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 	
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk = pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+	post = get_object_or_404(Post, pk = pk)
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'blog/post_detail.html', {'post': post})
 
 @login_required	
 def post_new(request):
